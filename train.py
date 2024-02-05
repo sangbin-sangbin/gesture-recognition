32f6f3f6f4140acc1cb35955cf83be53328bfab2
sangbin-sangbin <sangbin.park@intel.com>
2024-02-05 11:51:08 +0900

    1

--- train.py
+++ train.py
@@ -31,12 +31,11 @@ model = Model(INPUT_SIZE, HIDDEN_DIM, TARGET_SIZE)
 loss_function = nn.CrossEntropyLoss()
 optimizer = optim.SGD(model.parameters(), lr=0.1)
  
-dataset = json.load(open('dataset.json'))
+dataset = json.load(open('dataset2.json'))
 random.shuffle(dataset)
-train_dataset = dataset[:int(len(dataset)*0.7)]
-eval_dataset = dataset[int(len(dataset)*0.7):]
- 
-print(len(dataset))
+train_dataset = dataset[:int(len(dataset)*0.8)]
+val_dataset = dataset[int(len(dataset)*0.8):int(len(dataset)*0.9)]
+test_dataset = dataset[int(len(dataset)*0.9):]
  
 def print_progress_bar(iteration, total, time_per_step, prefix='', suffix='', length=30, fill='='):
     percent = ("{0:.1f}").format(100 * (iteration / float(total)))
@@ -48,10 +47,11 @@ def print_progress_bar(iteration, total, time_per_step, prefix='', suffix='', le
 total_epochs = 10
 total_steps = 100
 time_per_step = 0.01
+
+prev_loss = float('inf')
+tolerance = 3
  
-for epoch in range(total_epochs):
-    total_loss = 0.0  # Accumulate loss for each epoch
- 
+for epoch in range(total_epochs): 
     for data in train_dataset:
         model.zero_grad()
  
@@ -70,9 +70,7 @@ for epoch in range(total_epochs):
         loss.backward()
  
         optimizer.step()
- 
-        total_loss += loss.item()  # Accumulate loss value
- 
+  
     # Loop through steps with a progress bar
     for step in range(1, total_steps + 1):
         # Simulate some processing time (replace this with your actual training code)
@@ -80,18 +78,32 @@ for epoch in range(total_epochs):
  
         # Display the progress bar
         print_progress_bar(step, total_steps, time_per_step, prefix=f'Epoch {epoch}/{total_epochs}', length=30, fill='=')
-   
-    average_loss = total_loss / len(train_dataset)  # Calculate average loss for the epoch
-    print(f'\nAverage Loss: {average_loss:.4f}')
- 
-    # Add a new line after completing an epoch
-    print()
- 
+
+    val_loss = 0.0
+    for data in val_dataset:
+        landmarks = torch.tensor([element for row in data['landmarks'] for element in row], dtype=torch.float)
+        ans = [0 for _ in range(TARGET_SIZE)]
+        ans[data['gesture']] = 1
+        ans = torch.tensor(ans, dtype=torch.float)
+        res = model(landmarks)
+        loss = loss_function(res, ans)
+        val_loss += loss.item()  # Accumulate loss value
+
+    average_loss = val_loss / len(val_dataset)  # Calculate average loss for the epoch
+    print(f'\nValidation Loss: {average_loss:.4f}\n')
+
+    if prev_loss < average_loss:
+        tolerance -= 1
+        if tolerance == 0:
+            break
+
+    prev_loss = average_loss
+    
 print("Training completed.")
  
 good = 0
 bad = 0
-for data in eval_dataset:
+for data in test_dataset:
     res = list(model(torch.tensor([element for row in data['landmarks'] for element in row], dtype=torch.float)))
     if data['gesture'] == res.index(max(res)):
         good += 1
