7747e9b74921a0eeb5a6f20cd3aa77a41530d02a
sangbin-sangbin <sangbin.park@intel.com>
2024-02-06 16:47:11 +0900

    first hand tracking

--- test.py
+++ test.py
@@ -10,6 +10,36 @@ import subprocess
 import pygame
 
 
+def get_center(landmark):
+    sum_x = 0
+    sum_y = 0
+    for x, y in landmark:
+        sum_x += x
+        sum_y += y
+    return [sum_x / len(landmark), sum_y / len(landmark)]
+
+def same_hand_tracking(hands, prev_pos, same_hand_threshold):
+    if len(hands) == 0:
+        return -1, prev_pos
+
+    positions = []
+    for landmark in hands:
+        center = get_center(landmark)
+        positions.append([ center[0], center[1] ])
+
+    min_idx = -1
+    min_val = float('inf')
+    for i, [x, y] in enumerate(positions):
+        d = (x-prev_pos[0])**2 + (y-prev_pos[1])**2
+        if min_val > d:
+            min_idx = i
+            min_val = d
+
+    if min_val > same_hand_threshold:
+        return -1, prev_pos
+
+    return min_idx, [positions[min_idx][0], positions[min_idx][1]]
+
 def play_wav_file(file_path):
     pygame.mixer.init()
     try:
@@ -44,7 +74,7 @@ mp_hands = mp.solutions.hands
 mp_drawing = mp.solutions.drawing_utils
 
 # Initialize MediaPipe Hands model
-hands = mp_hands.Hands()
+hands = mp_hands.Hands(max_num_hands=5)
 
 # Open a webcam
 w = 1280
@@ -58,33 +88,16 @@ def nothing(x):
     
 cv2.namedWindow('gesture recognition')
 cv2.createTrackbar('time','gesture recognition',10,100,nothing)
+cv2.createTrackbar('same_hand','gesture recognition',5,100,nothing)
 cv2.createTrackbar('skip_frame','gesture recognition',1,50,nothing)
 
 gestures = [ 'default', 'left', 'right', 'select', 'exit', 'none' ]
 gesture_num = 0
 
-time_threshold = cv2.getTrackbarPos('time','gesture recognition')/100 #0.1
-state = {'gesture':5, 'start_time':time.time(), 'prev_gesture':4}
+state = {'gesture':5, 'start_time':time.time(), 'prev_gesture':5}
 
 landmark_skip_frame = max(cv2.getTrackbarPos('skip_frame','gesture recognition'), 1) #3
 frame_num = 0
-
-def direction(a, b):
-    x = b[0] - a[0]
-    y = b[1] - a[1]
-    if x >= abs(y):
-        return 0
-    elif -x >= abs(y):
-        return 1
-    elif abs(x) < -y:
-        return 2
-    elif abs(x) < y:
-        return 3
-    else:
-        return 4
-
-def distance(a, b):
-    return sqrt((a[0]-b[0])**2 + (a[1]-b[1])**2)
 
 def normalize_points(points):
     min_x, min_y = points[0]
@@ -113,7 +126,8 @@ landmark_num = 0
 gesture_time = 0
 gesture_num = 0
 
-landmark = []
+recognizing_hands = []
+recognizing_hand = []
 text_a = ''
 cur_gesture = gestures[5]
 elapsed_time = '0'
@@ -121,11 +135,14 @@ prev_gesture = gestures[5]
 
 recognizing = False
 start_recognizing_time_threshold = 3
-no_gesture_time = time.time()
 stop_recognizing_time_threshold = 3
+last_hand_time = time.time()
+
+wake_up_state = []
 
 while cap.isOpened():    
     time_threshold = cv2.getTrackbarPos('time','gesture recognition')/100
+    same_hand_threshold = cv2.getTrackbarPos('same_hand','gesture recognition')/1000
 
     # Read a frame from the webcam
     ret, frame = cap.read()
@@ -143,69 +160,126 @@ while cap.isOpened():
         end = time.time_ns()//1000000
         landmark_time += end - start
         landmark_num += 1
-        # Extract hand landmarks if available
+    
         if results.multi_hand_landmarks:
-            no_gesture_time = time.time()
-            # Get the coordinates of the index fingertip (landmark index 8)
-            hand_idx = -1
+            right_hands = []
             for idx, hand in enumerate(results.multi_handedness):
-                if hand.classification[0].label == 'Left':
-                    hand_idx = idx
-                    break
-            if hand_idx > -1: 
-                landmark = results.multi_hand_landmarks[hand_idx].landmark
-                lst, scale = normalize_points(list(map(lambda x : [x.x, x.y], landmark)))
+                if hand.classification[0].label == 'Left' or hand.classification[0].label == 'Right':
+                    right_hands.append(list(map(lambda x : [x.x, x.y], results.multi_hand_landmarks[idx].landmark)))
+            recognizing_hands = right_hands
 
-                start = time.time_ns()//1000000
-                res = list(model(torch.tensor([element for row in lst for element in row], dtype=torch.float)))
-                end = time.time_ns()//1000000
-                gesture_time += end - start
-                gesture_num += 1
+            if recognizing:
+                #find closest hand
+                hand_idx, recognized_hand_prev_pos = same_hand_tracking(right_hands, recognized_hand_prev_pos, same_hand_threshold)
 
-                p = max(res)
-                gesture_idx = res.index(p) if p >= 0.9 else 5
-                text_a = f"{gestures[gesture_idx]} {int(p*100)}%"
+                if hand_idx != -1:
+                    last_hand_time = time.time()
 
-                cur_gesture = gestures[state['gesture']]
-                elapsed_time = str(round(time.time() - state['start_time'], 2))
-                prev_gesture = gestures[state['prev_gesture']]
+                    landmark = results.multi_hand_landmarks[hand_idx].landmark
+                    landmark_lst = list(map(lambda x : [x.x, x.y], landmark))
+                    recognizing_hand = landmark_lst
+                    lst, scale = normalize_points(landmark_lst)
 
-                if state['gesture'] == gesture_idx:
-                    if not recognizing:
-                        if time.time()-state['start_time'] > start_recognizing_time_threshold:
-                            print('start recognizing') 
-                            play_wav_file('./start.wav')
-                            recognizing = True
-                    elif time.time()-state['start_time'] > time_threshold:
-                        if gestures[state['gesture']] == 'right' and gestures[state['prev_gesture']] == 'default':
-                            subprocess.run('adb shell input tap 80 600', shell=True)
-                            print('right')
-                        elif gestures[state['gesture']] == 'left' and gestures[state['prev_gesture']] == 'default':
-                            subprocess.run('adb shell input tap 80 500', shell=True)
-                            print('left')
-                        elif gestures[state['gesture']] == 'select' and gestures[state['prev_gesture']] == 'default':
-                            subprocess.run('adb shell input tap 80 720', shell=True)
-                            print('select')
-                        elif gestures[state['gesture']] == 'exit' and gestures[state['prev_gesture']] == 'default':
-                            subprocess.run('adb shell input tap 80 820', shell=True)
-                            print('exit')
-                        state['prev_gesture'] = gesture_idx
+                    start = time.time_ns()//1000000
+                    res = list(model(torch.tensor([element for row in lst for element in row], dtype=torch.float)))
+                    end = time.time_ns()//1000000
+                    gesture_time += end - start
+                    gesture_num += 1
+
+                    p = max(res)
+                    gesture_idx = res.index(p) if p >= 0.9 else 5
+                    text_a = f"{gestures[gesture_idx]} {int(p*100)}%"
+
+                    cur_gesture = gestures[state['gesture']]
+                    elapsed_time = str(round(time.time() - state['start_time'], 2))
+                    prev_gesture = gestures[state['prev_gesture']]
+
+                    if state['gesture'] == gesture_idx:
+                        if time.time()-state['start_time'] > time_threshold:
+                            if gestures[state['gesture']] == 'right' and gestures[state['prev_gesture']] == 'default':
+                                subprocess.run('adb shell input tap 80 600', shell=True)
+                                print('right')
+                            elif gestures[state['gesture']] == 'left' and gestures[state['prev_gesture']] == 'default':
+                                subprocess.run('adb shell input tap 80 500', shell=True)
+                                print('left')
+                            elif gestures[state['gesture']] == 'select' and gestures[state['prev_gesture']] == 'default':
+                                subprocess.run('adb shell input tap 80 720', shell=True)
+                                print('select')
+                            elif gestures[state['gesture']] == 'exit' and (gestures[state['prev_gesture']] == 'default' or gestures[state['prev_gesture']] == 'left'):
+                                subprocess.run('adb shell input tap 80 820', shell=True)
+                                print('exit')
+                            state['prev_gesture'] = gesture_idx
+                    else:
+                        state = {'gesture':gesture_idx, 'start_time':time.time(), 'prev_gesture':state['prev_gesture']}
                 else:
-                    state = {'gesture':gesture_idx, 'start_time':time.time(), 'prev_gesture':state['prev_gesture']}
+                    #stop recognizing
+                    recognizing_hand = []
+                    text_a = ''
+                    if recognizing and time.time() - last_hand_time > stop_recognizing_time_threshold:
+                        print('stop recognizing')
+                        play_wav_file('./start.wav')
+                        recognizing = False
+                        state = {'gesture':5, 'start_time':time.time(), 'prev_gesture':5}
+
+                        cur_gesture = 'none'
+                        elapsed_time = '0'
+                        prev_gesture = 'none'
+            else:
+                delete_list = []
+                wake_up_hands = []
+                for right_hand in right_hands:
+                    lst, scale = normalize_points(right_hand)
+
+                    res = list(model(torch.tensor([element for row in lst for element in row], dtype=torch.float)))
+                    p = max(res)
+                    gesture_idx = res.index(p) if p >= 0.9 else 5
+                    if gestures[gesture_idx] == 'default':
+                        wake_up_hands.append(right_hand)
+
+                checked = [0 for _ in range(len(wake_up_hands))]
+                for i, [prev_pos, start_time] in enumerate(wake_up_state):
+                    hand_idx, prev_pos = same_hand_tracking(wake_up_hands, prev_pos, same_hand_threshold)
+                    if hand_idx == -1:
+                        delete_list = [i] + delete_list
+                    elif time.time()-start_time > start_recognizing_time_threshold:
+                        print('start recognizing') 
+                        recognized_hand_prev_pos = get_center(wake_up_hands[hand_idx])
+                        play_wav_file('./start.wav')
+                        recognizing = True
+                        wake_up_state = []
+                        break
+                    else:
+                        checked[hand_idx] = 1
+
+                if not recognizing:
+                    for i in delete_list:
+                        wake_up_state.pop(i)
+
+                    for i in range(len(checked)):
+                        if checked[i] == 0:
+                            wake_up_state.append([get_center(wake_up_hands[i]), time.time()])
         else:
-            landmark = []
+            #stop recognizing
+            recognizing_hands = []
+            recognizing_hand = []
             text_a = ''
-            if recognizing and time.time() - no_gesture_time > stop_recognizing_time_threshold:
+            if recognizing and time.time() - last_hand_time > stop_recognizing_time_threshold:
                 print('stop recognizing')
                 play_wav_file('./start.wav')
-                recognizing = False
-
-    for i in range(len(landmark)):
-        x = landmark[i].x * frame.shape[1]
-        y = landmark[i].y * frame.shape[0]
-
+                recognizing = False  
+                state = {'gesture':5, 'start_time':time.time(), 'prev_gesture':5}
+                
+                cur_gesture = 'none'
+                elapsed_time = '0'
+                prev_gesture = 'none'
+                  
+    for rh in recognizing_hands:
+        for x, y in rh:
+            # Draw a circle at the fingertip position
+            cv2.circle(frame, (int(x * frame.shape[1]), int(y * frame.shape[0])), 5, (0, 255, 0), -1)
+    for x, y in recognizing_hand:
         # Draw a circle at the fingertip position
-        cv2.circle(frame, (int(x), int(y)), 5, (0, 255, 0), -1)
+        cv2.circle(frame, (int(x * frame.shape[1]), int(y * frame.shape[0])), 5, (255, 0, 0), -1)
 
     # Print Gesture    
     cv2.putText(frame, text_a, (frame.shape[1] // 2 + 230, frame.shape[0] // 2 - 220), cv2.FONT_HERSHEY_SIMPLEX, 2, (0, 0, 255), 3)
@@ -221,14 +295,14 @@ while cap.isOpened():
         for j, cell in enumerate(data):
             x = 50 + cell_width * j + 30
             y = text_position[1] + 20 + i * cell_height + cell_height // 2 + 10
-            cv2.putText(frame, str(cell), (x, y), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 255, 0), 2)
+            cv2.putText(frame, str(cell), (x, y), cv2.FONT_HERSHEY_SIMPLEX, 1, (255, 0, 0), 2)
 
             if i == 0:
                 cv2.rectangle(frame, (50 + cell_width * j, text_position[1] + 20),
-                                (50 + cell_width * (j + 1), text_position[1] + 20 + cell_height), (0, 255, 0), 2)
+                                (50 + cell_width * (j + 1), text_position[1] + 20 + cell_height), (255, 0, 0), 2)
             else:
                 cv2.rectangle(frame, (50 + cell_width * j, text_position[1] + 20 + i * cell_height),
-                                (50 + cell_width * (j + 1), text_position[1] + 20 + (i + 1) * cell_height), (0, 255, 0), 2)
+                                (50 + cell_width * (j + 1), text_position[1] + 20 + (i + 1) * cell_height), (255, 0, 0), 2)
 
     # Display the output
     cv2.imshow('gesture recognition', frame)
